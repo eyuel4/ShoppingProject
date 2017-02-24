f2aa4f207f02b3bc9c44356f3ec66087919ea03a
Eyuel Taddese <eyuel.taddese4@gmail.com>
2017-02-23 22:43:36 -0500

    Update TestController.java
    Test For TestController Written

--- onlineshopping/src/main/java/com/fenast/onlineshooping/controller/TestController.java
+++ onlineshopping/src/main/java/com/fenast/onlineshooping/controller/TestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class TestController {
+	// Test Controller used to check if the requestMapping is working
 	@RequestMapping(method = RequestMethod.GET)
 	public String getTestPage(Model model) {
 		model.addAttribute("greeting", "Hello Welcome to Online Shopping");
